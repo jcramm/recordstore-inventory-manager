0454b0b0c03f512f9be0a4ca7d068bc66c223eda
joel cramm <joelmcramm@gmail.com>
2020-06-09 12:38:19 -0400

    UPDATE search results view to include the inventory numbers and uids [https://trello.com/c/OifhEc8z/42-the-album-view-should-include-inventory-with-uids]
    related to https://trello.com/c/OifhEc8z/42-the-album-view-should-include-inventory-with-uids

--- lib/views/album.rb
+++ lib/views/album.rb
@@ -1,5 +1,6 @@
 require './lib/models/album'
 require './lib/models/artist'
+require './lib/models/inventory_item'
 
 module Views
   class Album
@@ -11,16 +12,28 @@ module Views
 
     def render
       artist = Models::Artist.where(id: @album.artist_id).first
+      inventory = ""
+      inventory_items.each do |item|
+        inventory += render_item(item)
+      end
       <<-HEREDOC
         Artist: #{format_string(artist.name)}
         Album: #{format_string(@album.title)}
         Released: #{@album.year}
-
+        #{inventory}
       HEREDOC
     end
 
     def format_string(str)
       str.split.map(&:capitalize).join(' ')
+    end
+
+    def inventory_items
+      Models::InventoryItem.where(album_id: @album.id)
+    end
+
+    def render_item(item)
+      "#{item.format}(#{item.quantity}): #{item.id}\n\t"
     end
   end
 end
